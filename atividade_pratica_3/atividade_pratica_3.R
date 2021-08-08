bdf408ee6fe715dbda67e40d727809322385cab3
eltondantas <eltondantas@outlook.com.br>
2021-08-07 22:24:40 -0300

    Atividade prática 3

---
+++ atividade_pratica_3/atividade_pratica_3.R
@@ -0,0 +1,8 @@
+# Atividade Prática 3
+# Elton Dantas de Oliveira Mesquita
+
+# Dados das Unidades Acadêmicas no período 20.2
+install.packages("readxl")
+library(readxl)
+dados = read_excel("dadosUAPorPeriodo.xlsx")
+View(dados)
